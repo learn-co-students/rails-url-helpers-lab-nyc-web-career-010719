b4e094226a1f1e5e6caabc05d743b8e0976b1a9f
Sarah Shin <sarahsujin.shin@gmail.com>
2019-02-01 16:19:20 -0500

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,17 @@
 class StudentsController < ApplicationController
   before_action :set_student, only: :show
-  
+
   def index
     @students = Student.all
   end
 
   def show
+    @student = Student.find(params[:id])
+  end
+
+  def activate
+    set_student.update(active: !@student.active)
+    redirect_to student_path(@student)
   end
 
   private
@@ -13,4 +19,5 @@ class StudentsController < ApplicationController
     def set_student
       @student = Student.find(params[:id])
     end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Student.create([{first_name: "Sarah", last_name: "Shin"},{first_name: "Elbert", last_name: "Low", active: false}])
