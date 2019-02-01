3166cf0791711a58e0da2c86f3e037ff646acb13
davidfloyd91 <davidfloyd91@gmail.com>
2019-02-01 16:30:42 -0500

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,18 @@
 class StudentsController < ApplicationController
-  before_action :set_student, only: :show
-  
+  before_action :set_student, only: [:show, :activate]
+  skip_before_action :verify_authenticity_token
+
   def index
     @students = Student.all
   end
 
   def show
+  end
+
+  def activate
+    @student.active = !@student.active
+    @student.save
+    redirect_to student_path(@student)
   end
 
   private
---
+++ db/migrate/20190201191155_change_active_default_to_false_in_students.rb
@@ -0,0 +1,5 @@
+class ChangeActiveDefaultToFalseInStudents < ActiveRecord::Migration
+  def change
+    change_column_default :students, :active, from: true, to: false
+  end
+end
