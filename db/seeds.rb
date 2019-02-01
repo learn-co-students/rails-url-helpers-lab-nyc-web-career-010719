a8db7307840b89f8d61b8c4bf88d7ac40c99a819
cvolpe12 <christopher.volpe12@gmail.com>
2019-02-01 16:08:53 -0500

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'faker'
+
+10.times do
+  Student.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name)
+end
+#
+# s1 = Student.create(first_name: "chris", last_name: "volpe")
+# s2 = Student.create(first_name: "joe", last_name: "somebody")
+# s3 = Student.create(first_name: "jon", last_name: "snow")
+# s4 = Student.create(first_name: "harry", last_name: "potter")
+# s5 = Student.create(first_name: "katniss", last_name: "everdeen")
